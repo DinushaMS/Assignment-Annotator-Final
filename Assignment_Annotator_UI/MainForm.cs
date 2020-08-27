3c4b515857baf06a62e0ffb28daf446a8219a5cc
Rathnayaka Mudiyanselage Dinusha Madhushanka Senarathna <dinusha89.usjp@gmail.com>
2020-08-27 19:59:44 +0530

    Revert "test"
    This reverts commit c48c0fb244017bf81c64b3750a5362e98331ea08.

--- Assignment_Annotator_UI/MainForm.cs
+++ Assignment_Annotator_UI/MainForm.cs
@@ -37,7 +37,7 @@ namespace Assignment_Annotator_UI
             InitializeComponent();
 
             DATA.pageCount = 0;
-            updater = new SharpUpdater(Assembly.GetExecutingAssembly(), this, new Uri("https://raw.githubusercontent.com/DinushaMS/Assignment-Annotator-Final/master/Assignment_Annotator_UI/bin/Release/update.xml"));
+            updater = new SharpUpdater(Assembly.GetExecutingAssembly(), this, new Uri("https://raw.githubusercontent.com/DinushaMS/Assignment-Annotator-Final/master/Assignment_Annotator_UI/bin/Publish/update.xml"));
         }
 
         private void MainForm_KeyDown(object sender, KeyEventArgs e)
